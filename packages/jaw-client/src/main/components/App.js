e6dca265564b2e51d247ecf1f9a3e6ac7a5f61f8
w1nston <wallander.jonas@gmail.com>
2018-06-09 00:24:29 +0200

    Add minimal styling.

--- packages/jaw-client/src/main/components/App.js
+++ packages/jaw-client/src/main/components/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import { Link, Router } from '@reach/router';
+import { css } from 'emotion';
 import ApplicationShell from './ApplicationShell';
 import About from '../../pages/about/components/About';
 import Home from '../../pages/home/components/Home';
 
+const navStyle = css`
+  align-items: center;
+  display: flex;
+  height: 3.125rem;
+  justify-content: space-around;
+`;
+
+const mainStyle = css`
+  padding: .625rem 1.25rem;
+`;
+
 const App = () => (
   <ApplicationShell>
-    <nav>
+    <nav className={navStyle}>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
     </nav>
-    <main role="main">
+    <main className={mainStyle} role="main">
       <Router>
         <Home path="/" />
         <About path="/about" />
--- packages/jaw-client/src/main/components/ApplicationShell.js
+++ packages/jaw-client/src/main/components/ApplicationShell.js
@@ -1,11 +1,26 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { css } from 'emotion';
+
+const headerStyle = css`
+  align-items: center;
+  background-color: #247BA0;
+  color: #fffcff;
+  display: flex;
+  height: 4.75rem;
+  justify-content: center;
+  width: 100%;
+`;
 
 const ApplicationShell = ({ children }) => (
-  <Fragment>
-    <header />
+  <section>
+    <header className={headerStyle}>
+      <span>
+        jonas<b>AmsenWallander</b>
+      </span>
+    </header>
     {children}
     <footer />
-  </Fragment>
+  </section>
 );
 
 export default ApplicationShell;
