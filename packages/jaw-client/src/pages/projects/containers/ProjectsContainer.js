f511ab6ff63e4b637722229679f4a943ab3f7efa
w1nston <wallander.jonas@gmail.com>
2018-08-08 11:16:54 +0200

    Add links to subdomains for projects.

--- packages/jaw-client/src/pages/projects/containers/ProjectsContainer.js
+++ packages/jaw-client/src/pages/projects/containers/ProjectsContainer.js
@@ -3,7 +3,7 @@ import Projects from '../components/Projects';
 
 const projects = [
   {
-    link: '/projects/upsndowns',
+    link: 'https://upsndowns.amsenwallander.se',
     title: 'Snakes and ladders',
     description: [
       'An implementation of the old board game Snakes and ladders.',
@@ -12,7 +12,7 @@ const projects = [
     sourceCodeLocations: ['https://github.com/w1nston/upsndowns'],
   },
   {
-    link: '/projects/punk-food',
+    link: 'https://punk-food.amsenwallander.se',
     title: 'Punk Food',
     description: [
       'List of dishes, with a link to Google search for a recipe, and suggestion of what beer goes well with the dish.',
