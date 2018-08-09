10281641b456151236913b6ef42afb80e9cf84d1
w1nston <wallander.jonas@gmail.com>
2018-08-09 23:09:21 +0200

    Add nicer loading spinner.

--- packages/jaw-client/src/common/components/Loading.js
+++ packages/jaw-client/src/common/components/Loading.js
@@ -1,5 +1,45 @@
 import React from 'react';
+import { css, keyframes } from 'emotion';
 
-const Loading = () => <p>Loading...</p>;
+const doubleBounceStyle = css`
+  height: 40px;
+  margin: 40px auto;
+  position: relative;
+  width: 40px;
+`;
+
+const doubleBounce = keyframes`
+  0%,
+  100% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1);
+  }
+`;
+
+const doubleBounceChildStyle = css`
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  background-color: #333;
+  opacity: 0.6;
+  position: absolute;
+  top: 0;
+  left: 0;
+  animation: ${doubleBounce} 2s infinite ease-in-out;
+`;
+
+const doubleBounceTwoStyle = css`
+  ${doubleBounceChildStyle}
+  animation-delay: -1s;
+`;
+
+const Loading = () => (
+  <div className={doubleBounceStyle}>
+    <div className={doubleBounceChildStyle} />
+    <div className={doubleBounceTwoStyle} />
+  </div>
+);
 
 export default Loading;
--- packages/jaw-client/src/pages/home/components/Home.js
+++ packages/jaw-client/src/pages/home/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const Home = () => <section></section>;
+const Home = () => <section />;
 
 export default Home;
