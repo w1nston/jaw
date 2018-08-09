78393cdf4660d4625c136919d69cfbb75eb23c15
w1nston <wallander.jonas@gmail.com>
2018-08-09 10:18:13 +0200

    Add ServiceWorker.

--- packages/jaw-client/src/index.js
+++ packages/jaw-client/src/index.js
@@ -2,5 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './main/components/App';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+registerServiceWorker();
