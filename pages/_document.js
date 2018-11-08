3ef1edc3991b1563dcb5fe6d67d774ed591549b9
w1nston <wallander.jonas@gmail.com>
2018-11-08 21:48:58 +0100

    Rename _document class.

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Document, { Head, Main, NextScript } from "next/document";
 import { extractCritical } from "emotion-server";
 
-export default class MyDocument extends Document {
+export default class JAWDocument extends Document {
   static getInitialProps({ renderPage }) {
     const page = renderPage();
     const styles = extractCritical(page.html);
