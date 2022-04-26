ece4927b4675e35b37780f982aa5c1505fe75e12
w1nston <wallander.jonas@gmail.com>
2022-04-26 22:28:19 +0200

    Add something on index page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,15 @@
 export default function Index() {
   return (
     <div>
-      <h1>Hmm...</h1>
+      <h1>Hi,</h1>
+      <p>
+        My name is Jonas, and I work as a Software Developer in Gothenburg,
+        Sweden.
+      </p>
+      <p>
+        Welcome to my playground where I intend to play around with things I
+        find interesting.
+      </p>
     </div>
   );
 }
