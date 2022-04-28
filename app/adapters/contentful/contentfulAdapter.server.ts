37b3babda8e315fa6d54ae5654405721eaf8888f
w1nston <wallander.jonas@gmail.com>
2022-04-28 11:16:37 +0200

    Place all contentful related logic in adapter

--- app/adapters/contentful/contentfulAdapter.server.ts
+++ app/adapters/contentful/contentfulAdapter.server.ts
@@ -1,5 +1,49 @@
 import { marked } from 'marked';
-import type { Thought, ThoughtMetadata } from '~/types/thoughts';
+import { createApi } from '~/libs/apis/apiFactory.server';
+import type { Api } from '~/libs/apis/apiFactory.server';
+import type { Thought, ThoughtApi, ThoughtMetadata } from '~/types/thoughts';
+import type { GetThoughtFn, GetThoughtsFn } from '../../types/thoughts';
+
+declare global {
+  let CONTENTFUL_BASE_URL: string;
+  let CONTENTFUL_DELIVERY_API_TOKEN: string;
+  let CONTENTFUL_SPACE_ID: string;
+}
+
+function createGetThoughts(api: Api): GetThoughtsFn {
+  return async function getThoughts(): Promise<ThoughtMetadata[]> {
+    let entries = await api.get(
+      `/spaces/${CONTENTFUL_SPACE_ID}/environments/master/entries?content_type=jawThought`
+    );
+
+    return transformThoughts(entries);
+  };
+}
+
+function createGetThought(api: Api): GetThoughtFn {
+  return async function getPost(id: string): Promise<Thought> {
+    let entry = await api.get(
+      `/spaces/${CONTENTFUL_SPACE_ID}/environments/master/entries/${id}`
+    );
+
+    return transformThought(entry);
+  };
+}
+
+export function createContentfulThoughtsApi(): ThoughtApi {
+  let options = {
+    headers: new Headers({
+      Authorization: `Bearer ${CONTENTFUL_DELIVERY_API_TOKEN}`,
+    }),
+  };
+
+  let api = createApi(CONTENTFUL_BASE_URL, options);
+
+  return {
+    getThoughts: createGetThoughts(api),
+    getThought: createGetThought(api),
+  };
+}
 
 export function transformThoughts(entries: any): ThoughtMetadata[] {
   return entries.items.map((item: any) => {
--- app/libs/apis/thoughtsApi.server.ts
+++ app/libs/apis/thoughtsApi.server.ts
@@ -1,57 +1,10 @@
-import {
-  transformThought,
-  transformThoughts,
-} from '~/adapters/contentful/contentfulAdapter.server';
-import type {
-  GetThoughtFn,
-  GetThoughtsFn,
-  Thought,
-  ThoughtApi,
-  ThoughtMetadata,
-} from '~/types/thoughts';
-import { createApi, Api } from './apiFactory.server';
-
-declare global {
-  let CONTENTFUL_BASE_URL: string;
-  let CONTENTFUL_DELIVERY_API_TOKEN: string;
-  let CONTENTFUL_SPACE_ID: string;
-}
+import { createContentfulThoughtsApi } from '~/adapters/contentful/contentfulAdapter.server';
+import type { ThoughtApi } from '~/types/thoughts';
 
 let api: ThoughtApi | null = null;
 
-function createGetThoughts(api: Api): GetThoughtsFn {
-  return async function getThoughts(): Promise<ThoughtMetadata[]> {
-    let entries = await api.get(
-      `/spaces/${CONTENTFUL_SPACE_ID}/environments/master/entries?content_type=blogPost`
-    );
-
-    return transformThoughts(entries);
-  };
-}
-
-function createGetThought(api: Api): GetThoughtFn {
-  return async function getPost(id: string): Promise<Thought> {
-    let entry = await api.get(
-      `/spaces/${CONTENTFUL_SPACE_ID}/environments/master/entries/${id}`
-    );
-
-    return transformThought(entry);
-  };
-}
-
 function createThoughtsApi(): ThoughtApi {
-  let options = {
-    headers: new Headers({
-      Authorization: `Bearer ${CONTENTFUL_DELIVERY_API_TOKEN}`,
-    }),
-  };
-
-  let api = createApi(CONTENTFUL_BASE_URL, options);
-
-  return {
-    getThoughts: createGetThoughts(api),
-    getThought: createGetThought(api),
-  };
+  return createContentfulThoughtsApi();
 }
 
 export function getThoughtsApi(): ThoughtApi {
