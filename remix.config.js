ff9c3515e89f71bdadacf03ec9441df68a86a100
w1nston <wallander.jonas@gmail.com>
2022-12-10 23:11:01 +0100

    update remix et al

--- remix.config.js
+++ remix.config.js
@@ -1,11 +1,9 @@
-/**
- * @type {import('@remix-run/dev').AppConfig}
- */
+/** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   serverBuildTarget: "cloudflare-workers",
   server: "./server.js",
   devServerBroadcastDelay: 1000,
-  ignoredRouteFiles: [".*"],
+  ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
