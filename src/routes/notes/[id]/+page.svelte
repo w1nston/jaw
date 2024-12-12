e43488ebdf4f6586d562d3deb302c9edace0a642
Jonas Amsen-Wallander <jonas@lvira.dev>
2024-12-12 21:38:16 +0100

    for reference prismjscss

---
+++ src/routes/notes/[id]/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import sanitizeHtml from 'sanitize-html';
+  import type { PageData } from './$types';
+  import { onMount } from 'svelte';
+
+  let { data }: { data: PageData } = $props();
+
+  let note = sanitizeHtml(data.note.content);
+
+  onMount(() => {
+    // @ts-ignore
+    Prism.highlightAll();
+
+    console.log('note?', note);
+  });
+</script>
+
+<svelte:head>
+  <link rel="stylesheet" href="/prism.css" />
+  <script src="/prism.js"></script>
+</svelte:head>
+
+<section>
+  {@html note}
+</section>
+
+<style>
+  section {
+    padding: 0 20px 20px 20px;
+  }
+</style>
