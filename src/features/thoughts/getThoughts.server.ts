ab64386bc6a770986ab7b7f9c4eb17e8535fc9dd
Jonas Amsen-Wallander <jonas@lvira.dev>
2025-01-03 09:56:14 +0100

    Remove thoughts stuff

--- src/features/thoughts/getThoughts.server.ts
+++
@@ -1,7 +0,0 @@
-import { getThoughtsApi } from '$lib/apis/thoughtsApi.server';
-import type { ThoughtMetadata } from '../../types/thoughts';
-
-export function getThoughts(): Promise<ThoughtMetadata[]> {
-  let thoughtsApi = getThoughtsApi();
-  return thoughtsApi.getThoughts();
-}
--- src/routes/thoughts/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import { getThoughts } from '../../features/thoughts/getThoughts.server';
-import type { PageServerLoad } from './$types';
-
-export let load: PageServerLoad = async ({ params }) => {
-  return {
-    thoughts: await getThoughts()
-  };
-};
-
